151f9635e24e3f865d827e86e8b82f316d3815f3
ErikJermanis <erik55jermanis@gmail.com>
2022-04-29 15:10:32 +0200

    ready

--- app/routes/list/index.tsx
+++ app/routes/list/index.tsx
@@ -1,6 +1,3 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-
 type Host = {
   id: number;
   name: string;
@@ -16,30 +13,12 @@ type Unconf = {
 type UnconfArr = Array<Unconf>;
 type UnconfCardProps = { unconf: Unconf };
 
-export const loader: LoaderFunction = async () => {
-  const data: UnconfArr = await (await fetch('https://ritz.selectnull.com/api/events')).json();
-
-  return data;
-};
-
-const UnconfCard = ({ unconf }: UnconfCardProps) => (
-  <li className="mt-4">
-    <div className="text-gray-900 font-bold text-xl text-center">{unconf.title}</div>
-    <div className="text-gray-700">{unconf.datetime} | @{unconf.host.name}</div>
-  </li>
-);
-
 const ListIndex = () => {
-  const events = useLoaderData<UnconfArr>();
 
   return (
     <>
       <h1 className="text-4xl text-center font-black my-8">UnConference List</h1>
       <main>
-        <ul className="flex flex-col items-center">
-          {events.map(event => <UnconfCard key={event.id} unconf={event} />)}
-        </ul>
-        <Link to="/">home</Link>
       </main>
     </>
   )
---
+++ app/styles/classes.js
@@ -0,0 +1,4 @@
+const apiUrl = 'https://ritz.selectnull.com/api/events';
+const unconfCardTitle = 'text-gray-900 font-bold text-xl text-center';
+const formInput = 'margin-auto border-2 border-gray-600 rounded-lg p-0.5';
+const formButton = 'mt-4 bg-cyan-700 px-2 py-1 rounded-lg border border-cyan-700 text-white hover:text-cyan-900 hover:bg-white duration-150';
